211b9215dccd1faea934aba8712931a8fd6d93a2
Théo Migeat <165766981+Bipeo-dev@users.noreply.github.com>
2025-12-11 18:42:14 +0100

    add better hover zone and animation

--- src/WaveJoinOrganization.jsx
+++ src/WaveJoinOrganization.jsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react"
-import styles from './WaveJoinOrganization.module.scss';
+import React, { useState } from "react";
+import styles from "./WaveJoinOrganization.module.scss";
 import NyassoIdol from "./assets/Nyasso_Idol.png";
 
 import { Link } from "react-router-dom";
 
 function WaveJoinOrganization() {
-  return (
-    <div style={{ position: "relative" }}>
-      {/* <svg className={styles['waveJoinOrganization-1']} width="0" height="0">
-          <defs>
-              <mask id="waveMask" maskUnits="userSpaceOnUse" x="0" y="0" width="1920" height="272">
-                  <rect width="1920" height="272" fill="black" />
-                  <path
-                      d="M697 120C372.782 112.617 72 161.5 -38 191L-40.5 202L-51.5 0.5H1996.5V191C1578.5 379.5 1202 131.5 697 120Z"
-                      fill="white"
-                  />
-              </mask>
-          </defs>
-      </svg> */}
+  const [hover, setHover] = useState(false);
 
-      <Link to={`/adhesion`} className={styles["waveJoinOrganization-link"]}>
+  return (
+    <div style={{ position: "relative" }} className={`${hover ? styles.isHovered : ""}`}>
+      <Link 
+        to="/adhesion" 
+        className={styles["waveJoinOrganization-clickable"]}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
         <div className={styles["waveJoinOrganization-clip"]}>
           <img className={styles["idolCat"]} src={NyassoIdol} />
         </div>
+
         <svg
           className={styles["waveJoinOrganization-2"]}
           preserveAspectRatio="none"
@@ -33,17 +29,21 @@ function WaveJoinOrganization() {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
+            className={styles.wavePath}
             d="M697 125C372.782 117.617 72 166.5 -38 196L-40.5 207L-51.5 5.5H1996.5V196C1578.5 384.5 1202 136.5 697 125Z"
             fill="#83DBD3"
             stroke="white"
-            stroke-width="10"
+            strokeWidth="10"
           />
         </svg>
-      </Link>
 
-      <img className={styles["adhesion-text"]} src={"./adhesion_text_fr.png"} />
+        <img
+          className={styles["adhesion-text"]}
+          src={"./adhesion_text_fr.png"}
+        />
+      </Link>
     </div>
   );
 }
 
-export default WaveJoinOrganization
+export default WaveJoinOrganization;
